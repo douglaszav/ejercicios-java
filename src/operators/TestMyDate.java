5111f09f05da1e8e245c5dce8cf55927941a7f90
Zavala <douglas.zavala@accenture.com>
2021-07-30 13:10:35 -0500

    modificación del archivo README

--- src/operators/TestMyDate.java
+++ src/operators/TestMyDate.java
@@ -3,26 +3,29 @@ package operators;
 public class TestMyDate {
     public static void main(String[] args) {
 
-        MyDate m1 = new MyDate(2,8,2019);
-        MyDate m2 = new MyDate(2,8,2019);
+        MyDate m1 = new MyDate(2, 8, 2019);
+        MyDate m2 = new MyDate(2, 8, 2019);
         MyDate m3 = null;
 
-        if(m1==m2) {
+        if (m1 == m2) {
             System.out.println("m1==m2");
-        }else {
+        } else {
             System.out.println("m1!=m2");
         }
 
-        if(m1.equals(m2)) {
+        if (m1.equals(m2)) {
             System.out.println("m1 is equal to m2");
-        }else {
+        } else {
             System.out.println("m1 is not equal to m2");
         }
-
-// Compare if m3 is equals to m1 or m2
-        if(m1.equals(m3) || m2.equals(m3)) {
-            System.out.println("m3 is equal to m1 or m2");
-        }else {
+        
+        try {
+            if (m3.equals(m1) || m3.equals(m2)) {
+                System.out.println("m3 is equal to m1 or m2");
+            } else {
+                System.out.println("m3 is not equal to m1 or m2");
+            }
+        } catch (Exception e) {
             System.out.println("m3 is not equal to m1 or m2");
         }
     }
@@ -63,8 +66,9 @@ class MyDate{
         this.year = year;
     }
 
+    @Override
     public boolean equals(Object obj) {
-        if (obj == null){return false;}
+        //if (obj == null){return false;}
         if (obj instanceof MyDate){
             MyDate x = (MyDate) obj;
             if(this.day == x.day && this.month == x.month && this.year == x.year){
